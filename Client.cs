710cd9be66ac09eaef8f65be410da67763ddee39
Kaverlio <49869432+Kaverlio@users.noreply.github.com>
2021-01-18 23:21:13 +0200

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net.Sockets;
+using System;
+using System.Net;
+using System.IO;
+
+public class Client : MonoBehaviour {
+	MySqlDB dostupDB = new MySqlDB();
+
+	public Button btnLogIn, btnConnect, btnSend;
+	public InputField enterLogin, hostInput, portInput, passwdInput;
+
+	public GameObject chatContainer;
+	public GameObject messagePrefab;
+	public string nameClient, passwdClient;
+
+	private bool socketReady;
+	private TcpClient socket;
+	private NetworkStream stream;
+	private StreamWriter writer;
+	private StreamReader reader;
+
+	void Start(){
+		 
+		btnLogIn.onClick.AddListener (delegate {
+			Debug.LogError("Clik");
+			nameClient = enterLogin.text.ToString();
+			passwdClient = passwdInput.text.ToString();
+			bool f = dostupDB.addClient(nameClient, passwdClient);
+			if (f == true) {
+			hostInput.gameObject.SetActive(true);
+			portInput.gameObject.SetActive(true);
+			btnConnect.gameObject.SetActive(true);
+			btnLogIn.gameObject.SetActive(false);
+			enterLogin.gameObject.SetActive(false);
+			passwdInput.gameObject.SetActive(false);
+			}
+		});
+	}
+		
+
+	public void ConnectToServer(){
+		// 
+		if (socketReady)
+			return;
+		//
+		string host = "127.0.0.1";
+		int port = 6321;
+
+		string h;
+		int p;
+		h = hostInput.GetComponentInChildren<Text>().text.ToString();
+		if (h != "")
+			host = h;
+		int.TryParse (portInput.GetComponentInChildren<Text>().text, out p);
+
+		if (p != 0)
+			port = p;
+
+		//
+		try{
+			socket = new TcpClient(host, port);
+			stream = socket.GetStream();
+			writer = new StreamWriter(stream);
+			reader = new StreamReader(stream);
+			socketReady = true;
+		}
+		catch(Exception e){
+			Debug.LogError("Socket error : " + e.Message);
+		}
+	}
+
+	private void Update(){
+		if (socketReady) {
+			if (stream.DataAvailable) {
+				string data = reader.ReadLine ();
+				if (data != null)
+					OnIncomingData (data);
+			}
+		}
+
+	}
+
+	private void OnIncomingData (string data){
+		if(data == "%NAME"){
+
+			Send ("&NAME|" + nameClient + "|" + passwdClient);
+			return;
+		}
+
+		GameObject go = Instantiate (messagePrefab, chatContainer.transform) as GameObject;	
+		go.GetComponentInChildren<Text> ().text = data;
+	}
+
+	private void Send(string data){
+
+		if (!socketReady)
+			return;
+		writer.WriteLine (data);
+		writer.Flush ();
+	}
+
+	public void OnSendButton(){
+		string message = GameObject.Find ("SendInput").GetComponent<InputField> ().text;
+		Send (message);
+		dostupDB.addLog (nameClient, message);
+	}
+
+	private void CloseSocket(){
+		if (!socketReady)
+			return;
+
+		writer.Close ();
+		reader.Close ();
+		socket.Close();
+		socketReady = false;
+	}
+
+	private void OnApplicationQuit(){
+		CloseSocket ();
+	}
+	private void OnDisable(){
+		CloseSocket ();
+	}
+
+}
---
+++ MySqlDB.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using System;
+using MySql.Data.MySqlClient;
+
+public class MySqlDB : MonoBehaviour {
+	private string connectSql = "server=37.59.55.185;user=3UWttL9edk;database=3UWttL9edk;password=B7WdnSUtrj";
+	bool fr;
+/*Username: VBu6ICLn7z
+Database name: VBu6ICLn7z
+Password: PjcbbdK5EO
+Server: remotemysql.com
+Port: 3306*/
+	public bool addClient (string login, string pass)
+	{
+		bool flag = true;
+		string maxIDclient = "SELECT MAX(ID) FROM clientInfo"; // id last client in DB sql
+		MySqlConnection conn = new MySqlConnection (connectSql);
+		conn.Open ();
+		int i = 0;
+		while(flag == true){
+			string selectLogin = "SELECT Login FROM clientInfo WHERE ID = " + i;
+			string selectPasswd = "SELECT Passwd FROM clientInfo WHERE ID = " + i;
+			MySqlCommand getLogin = new MySqlCommand (selectLogin, conn);
+			MySqlCommand getPasswd = new MySqlCommand (selectPasswd, conn);
+			MySqlCommand getID = new MySqlCommand (maxIDclient, conn);
+			string idClient = getID.ExecuteScalar ().ToString();
+			string LoginClient = getLogin.ExecuteScalar ().ToString ();
+			string PasswdClient = getPasswd.ExecuteScalar ().ToString ();
+			if (login != LoginClient) { 
+				if (i == System.Int32.Parse (idClient)) {
+					i++;
+					string addNewCl = "INSERT INTO clientInfo VALUES ('" + i + "', '" + login + "', '" + pass + "');"; // for create new client
+					MySqlCommand com = new MySqlCommand (addNewCl, conn);
+					com.ExecuteNonQuery ();
+					flag = false;
+					conn.Close ();
+					fr = true;
+				}
+			} else if (login == LoginClient && pass == PasswdClient) {
+				flag = false;
+				conn.Close ();
+				fr = true;
+			} else {
+				flag = false;
+				conn.Close ();
+				fr = false;
+			}
+			i++;
+		}
+		Debug.LogError (flag);
+		if (fr == true)
+			return true;
+		else
+			return false;
+	}
+	public void addLog(string nameCl, string mess){
+		int id;
+		string maxIDclient = "SELECT MAX(ID) FROM logDB";
+		string time = DateTime.Now.ToString ();
+		MySqlConnection conn = new MySqlConnection (connectSql);
+		conn.Open ();
+		MySqlCommand getID = new MySqlCommand (maxIDclient, conn);
+		string s = getID.ExecuteScalar ().ToString ();
+		if (s == "") {
+			id = 0;	
+		}
+		else
+		id = System.Int32.Parse(getID.ExecuteScalar ().ToString()) + 1;
+		
+		string sql = "INSERT INTO logDB VALUES ('"+ id + "', '" + nameCl + "', '" + mess + "', '" + time + "');";
+		MySqlCommand com = new MySqlCommand (sql, conn);
+		com.ExecuteNonQuery ();
+		conn.Close ();
+	}
+}
